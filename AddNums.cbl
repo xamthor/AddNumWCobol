c47de92497b5a3920b85c3430c4c11615f247945
xamthor <69559791+xamthor@users.noreply.github.com>
2020-08-14 13:03:36 -0500

    Implement Subtract Feature

--- AddNums.cbl
+++ AddNums.cbl
@@ -14,23 +14,27 @@
        01 NumOne PIC 99(20).
        01 NumTwo PIC 99(20).
        01 FinalNum PIC 99(1).
-       01 Back PIC X(30).
-       01 A PIC 99(2).
-
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
-            DISPLAY "Lets add twp numbers"
-
+            DISPLAY "Lets do some Math on two numbers"
+            DISPLAY "What type of operation would you like to perform"
+            DISPLAY "Add, Sub"
+            ACCEPT UserInput
+    
             DISPLAY "Enter First Number: "
             ACCEPT NumOne.
 
             DISPLAY "Enter Two Number: "
             ACCEPT NumTwo.
-
-            ADD NumOne,NumTwo TO FinalNum
-
-            DISPLAY "Holly Shizzz the two Numbers EQUAL ",  FinalNum
-
+            
+            IF UserInput EQUAL "Add"
+               ADD NumOne,NumTwo TO FinalNum
+               DISPLAY "Holly Shizzz the two Numbers EQUAL ",  FinalNum
+            ELSE IF UserInput EQUAL "Sub"
+               SUBTRACT NumOne FROM NumTwo
+               MOVE NumTwo to FinalNum
+               DISPLAY "Holly Shizzz the two Numbers EQUAL ",  FinalNum
+              
             STOP RUN.
 
        END PROGRAM Hello-NAME.
